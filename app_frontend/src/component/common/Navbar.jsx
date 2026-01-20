5cea9a8ced15d30165c16760b2c778891a12531e
Archit Kumar <architkumar314@gmail.com>
2026-01-21 00:57:47 +0530

    [Frontend]: Added Basic navbar

--- app_frontend/src/component/common/Navbar.jsx
+++ app_frontend/src/component/common/Navbar.jsx
@@ -1,1 +1,26 @@
+import React from 'react'
 
+function Navbar() {
+  return (
+    <div className='flex gap-100 justify-center text-green-300 bg-gray-800 p-5'>
+        
+        {/* Name section */}
+        <div>
+            <h1>~/archit-kumar</h1>
+        </div>
+
+        {/* Link section */}
+        <div className=''>
+            <ul className='flex gap-8'>
+                <li>home</li>
+                <li>about</li>
+                <li>skills</li>
+                <li>projects</li>
+                <li>contact</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
