951c5cb5b87f6568e208ef53b95a1094ee70add6
Archit Kumar <architkumar314@gmail.com>
2026-01-29 21:59:57 +0530

    [Frontend]: Added Initial Footer component.

---
+++ app_frontend/src/component/common/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className='text-green-500 text-xl flex justify-center p-6'>
+      <h1>{`{ "made_with": "❤️", "by": "Archit Kumar", "year": "2026" }`}</h1>
+    </div>
+  )
+}
+
+export default Footer
