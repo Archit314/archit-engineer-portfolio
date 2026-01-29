a721bb59332b807cf8327da4a9d14252307b43e5
Archit Kumar <architkumar314@gmail.com>
2026-01-29 23:29:30 +0530

    [Frontend]: Added components in App.jsx

--- app_frontend/src/App.jsx
+++ app_frontend/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
+import Footer from './component/common/Footer'
 import Navbar from './component/common/Navbar'
+import Terminal from './component/common/Terminal'
 import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
 
 function App() {
 
+  const [currentPage, setCurrentPage] = useState('home')
+
+  const loadPage = () => {
+
+    if(currentPage === 'home') {
+      return <Home/>
+    }
+    else if(currentPage === 'about'){
+      return <About/>
+    }
+    else if(currentPage === 'skills'){
+      return <Skills/>
+    }
+  }
+
   return (
-    <div>
-      <Navbar/>
-      <Home/>
+    <div className='bg-[linear-gradient(135deg,#0f0f1a_0%,#1a1a2e_50%,#16213e_100%)] min-h-screen'>
+      <Navbar setCurrentPage={setCurrentPage}/>
+      <main>
+        {loadPage()}
+      </main>
+      <Footer/>
     </div>
   )
 }
